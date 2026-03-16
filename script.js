452dc0b9029ef52f0e614c9e367f6643858fd4a7
navadeep <alamurunavadeep777@gmail.com>
2026-03-16 12:15:07 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,26 +1,68 @@
-async function getTemp(){
+const data = {
+temperature:42,
+duration:63,
+noise:25,
+lighting:75,
+fatigueIndex:33
+};
 
-let response = await fetch("http://localhost:3000/temperature");
-let data = await response.json();
+function createDoughnut(id,value,color){
 
-document.getElementById("temp").innerText = data.value;
+new Chart(document.getElementById(id),{
+
+type:'doughnut',
+
+data:{
+datasets:[{
+data:[value,100-value],
+backgroundColor:[color,'#2a3b4d']
+}]
+},
+
+options:{
+responsive:true,
+maintainAspectRatio:false,
+cutout:'70%',
+plugins:{
+legend:{display:false}
+}
+}
+
+});
 
 }
 
-async function getSound(){
+createDoughnut('tempChart',data.temperature,'#22c55e');
+createDoughnut('durationChart',data.duration,'#eab308');
+createDoughnut('noiseChart',data.noise,'#f97316');
+createDoughnut('lightChart',data.lighting,'#84cc16');
 
-let response = await fetch("http://localhost:3000/sound");
-let data = await response.json();
+new Chart(document.getElementById('fatiguePie'),{
 
-document.getElementById("sound").innerText = data.value;
+type:'pie',
 
+data:{
+datasets:[{
+data:[data.fatigueIndex,100-data.fatigueIndex],
+backgroundColor:['#eab308','#2a3b4d']
+}]
+},
+
+options:{
+responsive:true,
+maintainAspectRatio:false,
+plugins:{
+legend:{display:false}
+}
 }
 
-async function getLight(){
+});
 
-let response = await fetch("http://localhost:3000/light");
-let data = await response.json();
-
-document.getElementById("light").innerText = data.value;
-
+function updateTimestamp(){
+document.getElementById('lastUpdate').textContent =
+new Date().toLocaleTimeString();
 }
+
+updateTimestamp();
+
+setInterval(updateTimestamp,60000);
